2a47399dc0a4c1697a9ccf8cd6dc93123f80a499
Charity Cardinal <charcard@umich.edu>
2018-05-08 22:39:58 -0400

    Second part of lab

--- script.js
+++ script.js
@@ -5,16 +5,38 @@ $(document).ready(() => {
     $("#form").css("display","none");
 
     $(".circles").on("click", function(){
+        $(this).addClass("tracker");
         $("#form").css("display","block");
+        $("#form").fadeIn(150);
+        $("#form").attr("style", "display: flex");
+        $("#form").show();      
     });
-    $("button").on("click", function() {
-        $("#btn").css("display", "none")
+    $("#remove").on("click", function() {
+        $(".tracker").toggleClass("tracker");
+        $("#form").css("display", "none")
+    });
+    $("#btn").click(function(){
+        // let name = $("#name").val();
+        // let partySize = $("#party").val();
+        // // $("#name").attr("value", "");
+        $(".tracker").toggleClass("reserved available tracker");
+        $("#form").css("display", "none");
+        $("#form").fadeOut(150);
+        });
+    $(document).on("mouseenter", ".reserved" , function() {
+        let name = $("#name").val();
+        let partySize = $("#party").val();
+        $(`<p> Name: ${name}</p> <p> Party Size: ${partySize}</p>`).appendTo(".reserved > .reservation");
+    });
+    $(document).on("mouseleave", ".reserved", function() {
+        $(".reservation").hide();
+        $("#form").fadeOut(150);
+      });
 });
 
-    $(".circles").click(function(){
-        $(this).toggleClass("reserved available");
-});
 
-
-
-});
+//when the user inputs their information, circle is reserved  get input value of name and party number entered 
+//populate/put the value of their information into a form 
+//when moused over the form with the value of their information is displayed/shown
+//when moused off the form is hidden
+//should look good in mobile
